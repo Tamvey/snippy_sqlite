007b4daeaf216b205f358f47a982f42e6e8c14b9
matvey <matvey@DESKTOP-LA7H69G.>
2025-02-06 13:03:26 +0300

    Initial commit

---
+++ src/DBTable.cpp
@@ -0,0 +1,10 @@
+#include "../include/DBTable.h"
+#include "hpp/SQLiteDB.hpp"
+
+unsigned char RVDBAPI_VERSION_SYMBOL = RVDBAPI_CURRENT_INTERFACE_VERSION;
+
+extern const rvdb::RVDB_FunctionPointers RVDBAPI_ENTRY_POINT_SYMBOL = {
+    .initDatabase = &rvdb_initDatabase,
+    .getOperandsByRows = &rvdb_getOperandsByRows,
+    .closeDatabase = &rvdb_closeDatabase
+};
